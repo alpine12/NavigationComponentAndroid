69332c8be83b40d248a31b4df6ac5f6b8273ed1e
alpine12 <iqballecip@gmail.com>
2021-03-16 21:43:06 +0700

    Navigate fragment

---
+++ app/src/main/java/com/alpine12/navigationcomponent/LoginFragment.kt
@@ -0,0 +1,12 @@
+package com.alpine12.navigationcomponent
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
